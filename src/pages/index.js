fb021ec48c0966a84cb47c967f4cbabcab5975c6
Lucas Marie <lucas.marie.2b@gmail.com>
2019-10-06 21:43:23 -1000

    Removed date from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___title], order: DESC }) {
       edges {
         node {
           excerpt
@@ -64,7 +64,6 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
             title
             description
           }
